55a75a2b35cffc654685c0a6c7bea6155a3e823c
truongq <truongq@seagroup.com>
2018-03-09 10:05:49 +0700

    Optimize Reguar Expression and Trap rain water

--- src/programming/leetcode/TrapRainWater.java
+++ src/programming/leetcode/TrapRainWater.java
@@ -6,58 +6,27 @@ import java.util.PriorityQueue;
 /**
  * Created by quuynh on 15/06/17.
  */
-class Node {
-    int height, width;
-    Node left, right;
-
-    public Node(int height, int width) {
-        this.height = height;
-        this.width = width;
-    }
-}
-
-class NodeComparator implements Comparator<Node> {
-    public int compare(Node n1, Node n2) {
-        return n1.height - n2.height;
-    }
-}
 
 public class TrapRainWater {
     public int trap(int[] height) {
         int n = height.length;
-        PriorityQueue<Node> queue = new PriorityQueue<>(new NodeComparator());
-        Node[] nodes = new Node[n + 1];
-        for (int i = 0; i < n; i++) {
-            nodes[i] = new Node(height[i], 1);
+        if (n <= 2) return 0;
+        int[] left = new int[n];
+        left[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = Math.max(left[i - 1], height[i]);
         }
-        for (int i = 0; i < n; i++) {
-            if (i > 0) nodes[i].left = nodes[i - 1];
-            if (i < n - 1) nodes[i].right = nodes[i + 1];
-            queue.offer(nodes[i]);
+        int[] right = new int[n];
+        right[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = Math.max(right[i + 1], height[i]);
         }
         int result = 0;
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            if (node.left != null && node.right != null) {
-                int minHeight = Math.min(node.left.height, node.right.height);
-                if (minHeight >= node.height) {
-                    result += (minHeight - node.height) * node.width;
-                }
-                if (node.left.height == minHeight) {
-                    node.left.width += node.width;
-                } else {
-                    node.right.width += node.width;
-                }
-                node.left.right = node.right;
-                node.right.left = node.left;
+        for (int i = 1; i < n - 1; i++) {
+            if (left[i] > height[i] && right[i] > height[i]) {
+                result += Math.min(left[i], right[i]) - height[i];
             }
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        TrapRainWater solution = new TrapRainWater();
-        int[] heights = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        solution.trap(heights);
     }
 }
